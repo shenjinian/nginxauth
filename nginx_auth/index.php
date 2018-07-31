8aee31131a4fcdf283fb8b844834bbcda25d835d
Zhang Huanjie <james@ustc.edu.cn>
2018-07-31 09:21:56 +0800

    remove unsued code

--- nginx_auth/index.php
+++ nginx_auth/index.php
@@ -1,28 +1,7 @@
-<?php
+您好！
 
-$auth_key = "changeme";
+您从校外访问<?php echo $_REQUEST["next"]; ?>，需要先登录。
 
-session_start();
+请单击<a href=login.php>登录</a>，输入统一身份认证的用户名、密码，会继续下一步访问。
 
-if(isset($_SESSION['uid'])) 
-	$auth_uid=$_SESSION['uid'];
-else {
-	require_once('USTC_CAS.php');
-    	$cas = ustc_cas_login();
-    	$user = $cas->user();
-    	$gid = $cas->gid();
-    	$_SESSION['uid']=$user;
-    	$auth_uid=$user;
-}
 
-$expire_time = time()+3600*30;
-
-setcookie("nginx_auth_uid", $auth_uid, $expire_time, "/", $_SERVER['HTTP_HOST']);
-setcookie("nginx_auth_expire", $expire_time, $expire_time, "/", $_SERVER['HTTP_HOST']);
-setcookie("nginx_auth_hash", md5($auth_key."|".$auth_uid."|".$expire_time."|"), $expire_time, "/", $_SERVER['HTTP_HOST']);
-
-echo "hello $user<p>";
-
-$next=$_REQUEST["next"];
-echo "you are logged in, please go <a href=$next>$next</a>.";
-?>
--- nginxauth.lua
+++ nginxauth.lua
@@ -1,13 +1,9 @@
----- From https://github.com/StephenPCG/nginx-lua-simpleauth-module/blob/master/simpleauthn_cookie.lua
--- There are several options to make the authn stronger.
+---- Based from https://github.com/StephenPCG/nginx-lua-simpleauth-module/blob/master/simpleauthn_cookie.lua
 --   secret_key: a secret_key to protect bad user from forging fake authentication hash.
---   hash_func:  a function receives a string and output a digest string which will be stored
---               in user browser.
 --   auth_url_fmt: it is used to jump to real auth method on authn fail.
 
 local secret_key = 'please call nginxauth.set_secret_key() to change this key!'
 local auth_url_fmt = '/nginx_auth/?%s'
-local hash_func = ngx.md5
 
 local function set_secret_key (key)
     secret_key = key
@@ -17,18 +13,6 @@ local function set_auth_url_fmt (fmt)
     auth_url_fmt = fmt
 end
 
-local function set_hash_func (func)
-    hash_func = func
-end
-
-local function calc_hash(uid, expire, ...)
-    hashdata = secret_key .. '|' .. uid .. '|' .. expire .. '|'
-    for _, k in pairs({...}) do
-        hashdata = hashdata .. k .. '|'
-    end
-    return hash_func(hashdata)
-end
-
 local function get_uid (...)
     -- call this function to get authenticated uid, if not authenticated, return nil
     uid = ngx.var.cookie_nginx_auth_uid
@@ -36,7 +20,7 @@ local function get_uid (...)
     expire = ngx.var.cookie_nginx_auth_expire
     if uid ~= nil and hash ~= nil and expire ~= nil and
         ngx.req.start_time() < tonumber(expire) and
-        hash == calc_hash(uid, expire, ...) then
+        hash == ngx.md5(secret_key .. '|' .. uid .. '|' .. expire) then
             return uid
     end
     return nil
@@ -50,8 +34,27 @@ local function get_auth_url ()
     return string.format(auth_url_fmt, "next=" .. get_current_url())
 end
 
+local function ip2num (ip)
+    local o1,o2,o3,o4 = ip:match("(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)" )
+    return 2^24*o1 + 2^16*o2 + 2^8*o3 + o4
+end
+
+local ustc_ip = {
+"202.38.64.0", "202.38.95.255"
+}
+
 local function access (...)
-    -- do simple auth process, all 'logged in' will be authorized, 'non-logged in' user will
+    local headers=ngx.req.get_headers()
+    local clientip=headers["X-REAL-IP"] or headers["X_FORWARDED_FOR"] or ngx.var.remote_addr or "0.0.0.0"
+    ngx.log(ngx.ERR,"ip="..clientip)   
+    for i=1, #ustc_ip do
+       nginx.log(nginx.ERR,"i="..i)
+       if (ip2num(clientip)>=ip2num(ustc_ip[i]) and (ip2num(clientip)<=ip2num(ustc_ip[i+1]) then
+           nginx.log(nginx.ERR,"ustc_ip")
+           return
+       end
+       i = i + 1
+    end
     uid = get_uid(...)
     if uid == nil then
         ngx.header['Location'] = get_auth_url()
@@ -62,9 +65,6 @@ end
 local P = {
     set_secret_key = set_secret_key,
     set_auth_url_fmt = set_auth_url_fmt,
-    set_hash_func = set_hash_func,
-    get_auth_url = get_auth_url,
-    get_uid = get_uid,
     access = access
 }
 
